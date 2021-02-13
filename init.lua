3b2156691b10514d0d9974451c4f0885a0525393
Michael Lan <michaellan202@gmail.com>
2021-02-13 14:03:11 -0800

    still cant figure out truecolor in tmux

--- init.lua
+++ init.lua
@@ -25,12 +25,19 @@ mapk('t', '<Esc>', '<C-\\><C-n>', opts)
 vim.cmd 'au FileType fzf tno <Esc> <C-c>'
 
 vim.o.stl = ' %f %m%=%y '
+vim.wo.cursorline = true
 vim.wo.wrap = false
 
 vim.o.background = 'dark'
--- vim.o.t_8f = "\\<Esc>[38;2;%lu;%lu;%lum"
--- vim.o.t_8b = "\\<Esc>[48;2;%lu;%lu;%lum"
-vim.o.termguicolors = true
+
+vim.cmd [[
+if exists('+termguicolors')
+  let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
+  let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
+  set termguicolors
+endif
+]]
+
 
 vim.cmd 'let g:sneak#label = 1'
 
