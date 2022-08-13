f7184b6a5b9d276550f32dea9e122c33c83d8d2c
Michael Lan <michaellan202@gmail.com>
2022-08-13 09:43:59 -0700

    stuff

--- init.lua
+++ init.lua
@@ -13,29 +13,34 @@ P '~/Repositories/iswap.nvim'
 P 'https://github.com/nvim-treesitter/nvim-treesitter'
 P 'https://github.com/nvim-telescope/telescope.nvim'
 P 'https://github.com/TimUntersberger/neogit'
-P 'https://github.com/nvim-treesitter/nvim-treesitter-context'
+-- P 'https://github.com/nvim-treesitter/nvim-treesitter-context'
 P 'https://github.com/catppuccin/nvim'
 P 'https://github.com/lewis6991/gitsigns.nvim'
 P 'https://github.com/glepnir/lspsaga.nvim'
 P 'https://github.com/phaazon/hop.nvim'
 P 'https://github.com/ggandor/leap.nvim'
 P 'https://github.com/pechorin/any-jump.vim'
-P 'https://github.com/savq/melange'
-P 'https://github.com/aktersnurra/no-clown-fiesta.nvim'
-P 'https://github.com/folke/tokyonight.nvim'
 P '~/Code/longbow.nvim'
 P 'https://github.com/nvim-treesitter/playground'
 P 'https://github.com/folke/lua-dev.nvim'
+P 'rktjmp/lush.nvim'
+P 'https://github.com/rose-pine/neovim'
+P 'karb94/neoscroll.nvim'
 
 P.autoinstall(true)
 P.load()
 
-require('hop').setup{}
+vim.g.mapleader = ' '
+
+require('hop').setup {}
+require('neoscroll').setup{
+  easing_function = "quadratic"
+}
 
 vim.opt.termguicolors = true
 vim.opt.background = 'dark'
 vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
-require("catppuccin").setup{
+require("catppuccin").setup {
   integrations = {
     native_lsp = {
       enabled = true,
@@ -56,14 +61,17 @@ require("catppuccin").setup{
     gitsigns = true,
   }
 }
-vim.cmd [[colorscheme catppuccin]]
--- vim.cmd[[colorscheme tokyonight]]
--- vim.cmd[[colorscheme apprentice]]
+vim.opt.background = 'light'
+vim.cmd [[colorscheme rose-pine]]
 
 vim.cmd [[set ts=2 sw=2 sts=2 et]]
 
-require('lualine').setup{
-  options = { theme = 'catppuccin' }
+require('lualine').setup {
+  options = {
+    theme = 'rose-pine',
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = ''},
+  }
 }
 require('Comment').setup()
 require('colorizer').setup {
@@ -79,14 +87,16 @@ vim.opt.wrap = false
 vim.o.completeopt = 'menuone,noinsert,noselect'
 vim.o.shortmess = vim.o.shortmess .. 'c'
 
-local saga = require 'lspsaga'
-saga.init_lsp_saga({
-  max_preview_lines = 15,
-  code_action_icon = "",
+-- local saga = require 'lspsaga'
+-- saga.init_lsp_saga({
+--   max_preview_lines = 15,
+--   code_action_icon = "",
+-- })
 
-})
-
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
+vim.keymap.set("n", "<leader>scd", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
+vim.keymap.set("n", "[e", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+vim.keymap.set("n", "]e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 
@@ -103,11 +113,11 @@ local on_attach = function(client, bufnr)
     action.smart_scroll_with_saga(-1)
   end, { silent = true })
 
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
   vim.keymap.set("n", "<leader>pvd", "<cmd>Lspsaga preview_definition<CR>", { silent = true })
-  vim.keymap.set('n', 'K', "<cmd>Lspsaga hover_doc<CR>", bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
   vim.keymap.set('n', '<leader>sig', vim.lsp.buf.signature_help, bufopts)
   vim.keymap.set('n', '<leader>aws', vim.lsp.buf.add_workspace_folder, bufopts)
@@ -119,23 +129,23 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', '<leader>rn', "<cmd>Lspsaga rename<CR>", bufopts)
   vim.keymap.set('n', '<leader>ca', "<cmd>Lspsaga code_action<CR>", bufopts)
   vim.keymap.set('n', '<leader>gr', "<cmd>Lspsaga lsp_finder<CR>", bufopts)
-  vim.keymap.set('n', '<leader>f', vim.lsp.buf.formatting, bufopts)
+  vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
-require('lspconfig')['ocamllsp'].setup{
+require('lspconfig')['ocamllsp'].setup {
   on_attach = on_attach
 }
-require('lspconfig')['clangd'].setup{
+require('lspconfig')['clangd'].setup {
   on_attach = on_attach
 }
-require('lspconfig')['pyright'].setup{
+require('lspconfig')['pyright'].setup {
   on_attach = on_attach,
   cmd = { "pyright-langserver", "--stdio", "-v", "/Users/ml/GlobalVenv" }
 }
-require('lspconfig')['hls'].setup{
+require('lspconfig')['hls'].setup {
   on_attach = on_attach,
 }
-require('lspconfig')['sumneko_lua'].setup(require("lua-dev").setup{
+require('lspconfig')['sumneko_lua'].setup(require("lua-dev").setup {
   lspconfig = {
     on_attach = on_attach,
   }
@@ -154,15 +164,31 @@ endif
 
 vim.g.python3_host_prog = '~/GlobalVenv/bin/python3.9'
 
-require('nvim-treesitter.configs').setup{
+require('nvim-treesitter.configs').setup {
   highlight = { enable = true }
 }
 
-require('treesitter-context').setup{
-  enable = true
-}
+-- require('treesitter-context').setup{
+--   enable = true
+-- }
 
-require('gitsigns').setup()
+local function nc(keys, cmd)
+  vim.keymap.set("n", "<leader>" .. keys, "<cmd>" .. cmd .. "<cr>")
+end
+
+local function nl(keys, func)
+  vim.keymap.set("n", "<leader>" .. keys, func)
+end
+
+require('gitsigns').setup {
+  on_attach = function()
+    nc("sh", "Gitsigns stage_hunk")
+    nc("rh", "Gitsigns reset_hunk")
+    nc("nh", "Gitsigns next_hunk")
+    nc("ph", "Gitsigns prev_hunk")
+    nc("pvh", "Gitsigns preview_hunk")
+  end
+}
 
 vim.g.filetype_pl = 'perl'
 
@@ -170,14 +196,21 @@ vim.cmd [[au BufRead,BufNewFile *.pl setf perl]]
 
 require('leap').set_default_keymaps()
 
-require('neogit').setup{
+require('neogit').setup {
   disable_commit_confirmation = true,
   disable_insert_on_commit = false,
   use_magit_keybindings = true,
   signs = {
-    -- { CLOSED, OPENED }
     section = { "", "" },
     item = { "", "" },
     hunk = { "", "" },
   },
 }
+
+vim.g.neovide_cursor_vfx_mode = 'railgun'
+vim.opt.guifont = 'JetBrainsMono Nerd Font Mono:h22'
+
+nc("of", "Telescope oldfiles theme=ivy")
+nc("nt", "tabnew")
+nc("dt", "tabclose")
+nc("gg", "Neogit")
